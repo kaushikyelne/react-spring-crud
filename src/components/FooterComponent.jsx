0ef31dc02273f7a1b25eea2af0dabe76c98b81ad
kaushikyelne <kaushik.s.yelne@gmail.com>
2021-04-05 11:42:33 +0530

    footer media icons size increase

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -5,10 +5,10 @@ const FooterComponent = () => {
         <React.Fragment>
             <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"/>
             <footer className="footer">
-                <span className="text-muted">All Rights Reserved 2021 @KaushikYelne</span>
-                <a href="https://github.com/kaushikyelne" style={{marginLeft:"10px"}}><i class="fa fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/kaushik-yelne-4aa657170/" style={{marginLeft:"10px"}}><i class="fa fa-linkedin"></i></a>
-                <a href="https://stackoverflow.com/users/13632450/kaushik-yelne" style={{marginLeft:"10px"}}><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
+                <span >All Rights Reserved 2021 @KaushikYelne</span>
+                <a href="https://github.com/kaushikyelne" style={{marginLeft:"20px"}}><i class="fa fa-github fa-2x"></i></a>
+                <a href="https://www.linkedin.com/in/kaushik-yelne-4aa657170/" style={{marginLeft:"20px"}}><i class="fa fa-linkedin fa-2x"></i></a>
+                <a href="https://stackoverflow.com/users/13632450/kaushik-yelne" style={{marginLeft:"20px"}}><i class="fa fa-stack-overflow fa-2x" aria-hidden="true"></i></a>
             </footer>
         </React.Fragment>
     );
